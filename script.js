d34c52b42def80c052c135f200ab91621de292bb
Jayanta Ojha <jayantaojha686@gmail.com>
2024-05-04 01:12:30 +0530

    add useful comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// accessing elements from index.html file
 const h1 = document.querySelector('h1');
 const body = document.querySelector('body');
 const button = document.querySelector('#new-task button');
 const data = document.querySelector('#new-task input');
 let taskList = document.querySelector('#task-list');
 
+
+// list of colors for app theme
 const colors = [
     {
         gradient: 'linear-gradient(to right, #333333, #000000)', // black
@@ -52,6 +55,9 @@ const colors = [
     }
 ];
 
+
+// data simply represent new task
+// event listener for enter click
 data.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         const inputValue = data.value;
@@ -61,6 +67,7 @@ data.addEventListener('keypress', (e) => {
     }
 })
 
+// event listener for add button
 button.addEventListener('click', () => {
     const inputValue = data.value;
     if (inputValue !== null && inputValue !== "") {
@@ -70,25 +77,31 @@ button.addEventListener('click', () => {
 
 
 function addTask(inputValue) {
-    const div = document.createElement('div');
+    const div = document.createElement('div'); // for new task
+    const deleteIcon = div.querySelector('.fa-trash'); // for delete icon
+    const statusIcon = div.querySelector('.status-icon'); // to check done/undone a task
+    const taskString = div.querySelector('.task-string');
+
+
+    // styling for the new task
     div.className = "task";
     div.innerHTML = `<i class="fas fa-check-circle status-icon" style="cursor: pointer;"></i>
     <p class="task-string">${inputValue}</p>
     <i class="fa-solid fa-trash" style="color: red; cursor: pointer; margin-left: auto; margin-right: 5px; opacity: 0.3;"></i>`;
 
+
+    // add new task in the taskList on index.html
     taskList.appendChild(div);
     data.value = '';
 
-    const deleteIcon = div.querySelector('.fa-trash');
 
+    // event listener on delete icon for hover and click
     deleteIcon.addEventListener('mouseover', function () {
-        // Code to execute when the mouse enters (hovers over) the delete icon
         deleteIcon.style.opacity = "1";
     });
 
     deleteIcon.addEventListener('mouseout', function () {
-        // Code to execute when the mouse leaves the delete icon
-        deleteIcon.style.opacity = "0.3"; // You can set it back to the original opacity or any other value
+        deleteIcon.style.opacity = "0.3";
     });
 
     deleteIcon.addEventListener('click', function () {
@@ -96,33 +109,35 @@ function addTask(inputValue) {
     });
 
 
-    const statusIcon = div.querySelector('.status-icon');
-    const taskString = div.querySelector('.task-string');
-
+    // to check done or undone a task
     statusIcon.addEventListener('click', () => {
         changeStatus(statusIcon, taskString, inputValue);
     });
+
 }
 
+// to change the status of a task (complete/ incomplete)
 function changeStatus(statusIcon, taskString, inputValue) {
     const currentColor = statusIcon.style.color;
 
     if (currentColor === "" || currentColor === "#333333" || currentColor === "rgb(51, 51, 51)") {
         statusIcon.style.color = "green";
         taskString.innerHTML = `<del><i style="color: grey;"> ${inputValue} </i></del>`;
-    } else if (currentColor === "green" || currentColor === "rgb(0, 128, 0)") {
+    } 
+    else if (currentColor === "green" || currentColor === "rgb(0, 128, 0)") {
         statusIcon.style.color = "#333333";
         taskString.innerHTML = `${inputValue}`;
     }
 }
 
 
+// Remove the task element from the task list
 function deleteTask(taskElement) {
-    // Remove the task element from the task list
     taskElement.remove();
 }
 
-// ----------------------------   for background color for the body --------------------------------------
+
+// for background color for the body
 
 // Debounce function to ensure the event is processed only once within a specific time frame
 function debounce(func, wait) {
@@ -160,7 +175,6 @@ const handleDoubleClick = debounce(() => {
 
 }, 300);
 
+
+// double click on the screen (change theme)
 document.addEventListener('dblclick', handleDoubleClick);
-
-
-// ------------------------ for long-press -> settings open ----------------------------------
